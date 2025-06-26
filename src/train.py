a347877764ad6c23c6098742b9eea94dcecf0d3d
rishabh <rishabh.srivastava@phot.ai>
2025-06-26 05:45:29 +0530

    added code for training

--- src/train.py
+++ src/train.py
@@ -12,14 +12,16 @@ with open("assets/layout.json", "r") as f:
     layout_template = json.load(f)
 
 
-def json_dataset(data):
+def json_dataset(data, fontFamilyList:list[str]):
     conversations=[]
     for item in data:
         item_input = item['input']
         product_name = item_input.get("product_name", "")
         product_details = item_input.get("product_details", "")
         product_price = item_input.get("product_price", "")
+        product_color = item.get("product_color", "")
         layout = item_input.get("layout", "centered_hero")
+
         important_fields = {
             "svg": ["top", "left", "width", "height", "src", "id"],
             "text": ["top", "left", "width", "height", "fill", "text", "fontSize", "fontFamily", "textAlign", "id"],
@@ -38,11 +40,15 @@ def json_dataset(data):
 
         layout_description = " ".join(layout_template[layout])
 
-        input_text = f'I want you to create a beautiful advertisement banner, following the best practices in form of condensed fabricjs json for the given product with the following details:\n\n##Product Details:\n\n **Product Name:** {product_name}\n **Product Description:** {product_details}\n **Product Price:** {product_price}\n\n. The banner should follow the {layout} layout\n\ ###Layout Description:\n\n {layout_description}.\n\n Think step by step and then create the banner. '
+        input_text = f'I want you to create a beautiful advertisement banner, following the best practices in form of condensed fabricjs json for the given product with the following details:\n\n##Product Details:\n\n **Product Name:** {product_name}\n **Product Description:** {product_details}\n **Product Price:** {product_price}\n\n **Product Color:** {product_color}\n\n The banner should follow the {layout} layout\n ###Layout Description:\n\n {layout_description}.\n\n Think step by step and then create the banner. You must strictly choose fontFamily for the text layers from the following list: {json.dumps(fontFamilyList)}.'
+        
+        
         reasoning_text = f"Let me think step-by-step, The user wants me to create a banner in condensed fabric js format, which have following important fields for given layer type: \n{important_fields}\n\n I have to ensure that no two text layers overlap, and maintain proportional spacing between each layer to support a natural visual flow for the viewer. So I have to place layers carefully using *top*(y coordinate of top-left of the layer), *left*(x coordinate of top-left of the layer), *width*(width of the layer), *height*(height of the layer) keys. But for the text width and height don't have an effect in fabricjs, so I will place them correctly using  *top*, *left* and *fontSize* keys. The text are readable, with contrasting color to the background. Let me give an overview of the banner: \n\n"+ item['banner_details']+ "\nNow I will create the banner."
+        
+        
         output_text = f'{json.dumps(item["output"])}'
         conversations.append({"input":input_text,
-                              "output":f"\n<think>\n{reasoning_text}\n</think>\n FabricJS JSON:\n<json>{output_text}</json>"})
+                              "output":f"\n<think>\n{reasoning_text}\n</think>\n Here is your condensed FabricJS JSON:\n<json>{output_text}</json>"})
     df = Dataset.from_pandas(pd.DataFrame(conversations))
     return df
 def generate_conversation(df_data):
@@ -56,8 +62,8 @@ def generate_conversation(df_data):
         ])
     return { "conversations": conversations }
 
-def data_prep(data, tokenizer):
-    df_data=json_dataset(data)
+def data_prep(data, tokenizer, fontFamilyList:list[str]):
+    df_data=json_dataset(data, fontFamilyList)
     template_conversations = tokenizer.apply_chat_template(
     df_data.map(generate_conversation, batched = True)["conversations"],
     tokenize = False)
@@ -92,12 +98,12 @@ def load_model():
 def main():
     # Load data from environment
     model, tokenizer = load_model()
-    data_path = "training_data"
+    data_path = "final_data"
     training_data = []
     for file in os.listdir(data_path):
         with open(os.path.join(data_path, file), "r") as f:
             training_data.append(json.load(f))
-    dataset = data_prep(training_data, tokenizer)
+    dataset = data_prep(training_data, tokenizer, fontFamilyList=list(fonts.keys()))
     trainer = SFTTrainer(
         model=model,
         tokenizer=tokenizer,
@@ -117,9 +123,8 @@ def main():
             lr_scheduler_type="linear",
             save_strategy="steps",
             save_steps=100,
-            save_total_limit=50,
+            save_total_limit=100,
             report_to="wandb",
-            resume_from_checkpoint="/home/ubuntu/rishabh/llm_training/model/checkpoint-2104",
     ))
 
     # Start training
